f3b9ba48fe387ee1c41b40c632920d5217929316
Waratnan Suriyasorn <s.waratnan@gmail.com>
2016-06-02 21:58:52 +0700

    Initial Commit

---
+++ ReactiveSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveSwift
+//
+//  Created by Waratnan Suriyasorn on 6/2/2559 BE.
+//  Copyright © 2559 Chi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ReactiveSwiftUITests/ReactiveSwiftUITests.swift
@@ -0,0 +1,36 @@
+//
+//  ReactiveSwiftUITests.swift
+//  ReactiveSwiftUITests
+//
+//  Created by Waratnan Suriyasorn on 6/2/2559 BE.
+//  Copyright © 2559 Chi. All rights reserved.
+//
+
+import XCTest
+
+class ReactiveSwiftUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
