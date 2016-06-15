ff2ac65545d48899282dbf5c2b072dff21b86d86
Waratnan Suriyasorn <s.waratnan@gmail.com>
2016-06-16 00:35:20 +0700

    init

--- ReactiveSwift/ViewController.swift
+++ ReactiveSwift/ViewController.swift
@@ -7,19 +7,100 @@
 //
 
 import UIKit
+import ReactiveCocoa
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+	@IBOutlet weak var passwordTextField: UITextField!
+	@IBOutlet weak var usernameTextField: UITextField!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	@IBOutlet weak var signInButton: UIButton!
+	@IBOutlet weak var signInFailureText: UILabel!
 
+	var passwordIsValid = false
+	var usernameIsValid = false
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		updateUIState()
+
+		let validUsernameSignal = self.usernameTextField.rac_textSignal()
+			.map { text in
+
+				let value = text as! String
+				print(value)
+				let isValid = self.isValidUserName(value)
+				return isValid
+		}
+
+		let validPasswordSignal = self.passwordTextField.rac_textSignal()
+			.map { text in
+
+				let value = text as! String
+				let isValid = self.isValidPassword(value)
+				return isValid
+		}
+
+		validUsernameSignal.subscribeNext { [weak weakSelf = self](valid) in
+			let isValid = valid as! Bool
+			weakSelf?.usernameTextField.backgroundColor = weakSelf?.backgroundColorForValidState(isValid)
+		}
+
+		validPasswordSignal.subscribeNext { [weak weakSelf = self](valid) in
+			let isValid = valid as! Bool
+			weakSelf?.passwordTextField.backgroundColor = weakSelf?.backgroundColorForValidState(isValid)
+
+		}
+
+		let signUpActiveSignal = RACSignal.combineLatest([validUsernameSignal, validPasswordSignal])
+        
+		signUpActiveSignal.subscribeNext { [weak weakSelf = self](value) in
+
+			let tupe = value as! RACTuple
+			let isUserNameValid = tupe.first as! Bool
+			let isPwdValid = tupe.second as! Bool
+
+			weakSelf?.signInButton.enabled = (isUserNameValid && isPwdValid)
+		}
+
+		self.signInButton.rac_signalForControlEvents(.TouchUpInside).flattenMap { (id) -> RACStream! in
+			return self.signInSignal()
+		}.subscribeNext { [weak weakSelf = self](value) in
+
+			let success = value as! Bool
+			if success {
+				weakSelf?.performSegueWithIdentifier("signInSuccess", sender: self)
+			}
+		}
+	}
+
+	@IBAction func signInButtonTouched(sender: UIButton) {
+	}
+
+	private func isValidUserName(text: String) -> Bool {
+		return text.characters.count > 3
+	}
+
+	private func isValidPassword(text: String) -> Bool {
+		return text.characters.count > 3
+	}
+
+	private func updateUIState() {
+		self.signInButton.enabled = passwordIsValid && usernameIsValid
+	}
+
+	private func backgroundColorForValidState(valid: Bool) -> UIColor {
+		return valid ? UIColor.clearColor() : UIColor.redColor()
+	}
+
+	private func signInSignal() -> RACSignal
+	{
+		return RACSignal.createSignal { (subscriber) in
+			subscriber.sendNext(true)
+			subscriber.sendCompleted()
+			return nil
+		}
+	}
 }
 
